d3bbbfd5d9619571d36d9d9b70f187cd1d9f4c4a
Lars Vogel <Lars.Vogel@vogella.com>
2020-04-29 22:59:28 +0200

    Fixing the project names and adding a test plug-in to the example

---
+++ com.vogella.tasks.ui.tests/src/com/vogella/tycho/unittests/ExampleTest.java
@@ -0,0 +1,16 @@
+package com.vogella.tycho.unittests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+
+	@Test
+	public void test() {
+		// just an example
+		assertTrue(true);
+	}
+
+}
